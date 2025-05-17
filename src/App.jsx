8fc5dfcbe1de45bab516df7edeeca4e2580061ea
wany <waniprincesse@04gmail.com>
2025-05-17 05:42:36 +0100

    conception de la page d'accueil

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import './index.css'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -18,14 +19,14 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+        <button className='bg-blue-600' onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
-        <p>
+        <p className='text-blue-500 text-[23px] bg-blue-600'>
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
       </div>
-      <p className="read-the-docs">
+      <p className="read-the-docs text-blue-500">
         Click on the Vite and React logos to learn more
       </p>
     </>
