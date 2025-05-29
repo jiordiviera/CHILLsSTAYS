f10b96675b1085b37c1bba7d9e6c6b83ffa802e2
wany <waniprincesse@04gmail.com>
2025-05-29 18:43:16 +0100

    Creation de la page d'acceuil etudiant

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css'
 import Acceuil from './pages/Acceuil';
+import AcceuilEtudiant from './pages/AcceuilEtudiant';
 
 function App() {
 
@@ -11,6 +12,7 @@ function App() {
       <div className="">
         <Routes>
           <Route path="/" element={<Acceuil/>} />
+          <Route path="/acceuiletudiant" element={<AcceuilEtudiant/>} />
         </Routes>
       </div>
     </Router>
